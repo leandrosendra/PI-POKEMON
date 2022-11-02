e5abfffb36997745461fdfc7ace0303a0828c66b
leandrogbs <leandrosendra09@gmail.com>
2022-11-02 13:33:51 -0300

    css finished 2

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styles from '../stylesheets/Landing.module.css'
+import pokemon from '../images/pokemon.png'
 
 const LandingPage = () => {
     return (
-        <div>
-            <h1>Welcome to the Pokemon page</h1>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Welcome to the Pokemon App</h1>
+            <img src={pokemon} width='300px' height='200px' alt="poke" />
             <Link to='/home'>
-                <button>Ingresar</button>
+                <button className={styles.btn}>START</button>
             </Link>
         </div>
     );
